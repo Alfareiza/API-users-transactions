e6b3b7725f1f01e2773482c26b39065eac76acdd
Alfonso <alfareiza@gmail.com>
2022-05-28 16:00:27 -0300

    validators for input fields to serialize

---
+++ core/base/validators.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+
+def validate_amount(value):
+    if value['type'].lower() == 'outflow' and value['amount'] > 0:
+        raise serializers.ValidationError({'type': 'Outflow entries can not be positive.'})
+    elif value['type'].lower() == 'inflow' and value['amount'] < 0:
+        raise serializers.ValidationError({'type': 'Inflow entries can not be negative .'})
+
+
+def validate_reference(value):
+    if not value['reference'].isnumeric():
+        raise serializers.ValidationError({'reference': 'The reference only must contain numbers.'})
