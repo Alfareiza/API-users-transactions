7585b19d605b52e4e6ba67dca7da78ebd168bd15
Alfonso <alfareiza@gmail.com>
2022-06-01 13:23:53 -0300

    migrations for Transaction model

--- core/base/migrations/0001_initial.py
+++ core/base/migrations/0001_initial.py
@@ -7,24 +7,41 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Transaction',
+            name="Transaction",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('reference', models.CharField(max_length=6, unique=True)),
-                ('date', models.DateField()),
-                ('type', models.CharField(choices=[('outflow', 'Outflow'), ('inflow', 'Inflow')], default='outflow', max_length=10)),
-                ('amount', models.DecimalField(decimal_places=2, max_digits=12)),
-                ('category', models.CharField(max_length=15)),
-                ('user_email', models.EmailField(max_length=254)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("reference", models.CharField(max_length=6, unique=True)),
+                ("date", models.DateField()),
+                (
+                    "type",
+                    models.CharField(
+                        choices=[("outflow", "Outflow"), ("inflow", "Inflow")],
+                        default="outflow",
+                        max_length=10,
+                    ),
+                ),
+                (
+                    "amount",
+                    models.DecimalField(decimal_places=2, max_digits=12),
+                ),
+                ("category", models.CharField(max_length=15)),
+                ("user_email", models.EmailField(max_length=254)),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
             ],
             options={
-                'ordering': ('-date',),
+                "ordering": ("-date",),
             },
         ),
     ]
