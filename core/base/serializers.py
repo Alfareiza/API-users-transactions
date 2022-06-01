93ff71147cec5507fcc988bb157f6b12712836f5
Alfonso <alfareiza@gmail.com>
2022-06-01 13:28:32 -0300

    serializers for the DRF API

--- core/base/serializers.py
+++ core/base/serializers.py
@@ -1,3 +1,6 @@
+"""
+Serializers for transaction APIs
+"""
 from rest_framework import serializers
 
 from core.base.models import Transaction
@@ -5,12 +8,26 @@ from core.base.validators import validate_amount, validate_reference
 
 
 class TransactionsSerializer(serializers.ModelSerializer):
+    """Serializer for Transactions."""
+
     class Meta:
         model = Transaction
-        # fields = ['reference', 'date', 'type', 'amount', 'category', 'user_email']
-        exclude = ['id', 'created_at']
+        fields = [
+            "id",
+            "reference",
+            "date",
+            "type",
+            "amount",
+            "category",
+            "user_email",
+        ]
+        # exclude = ['id', 'created_at']
+        validators = [validate_amount, validate_reference]
 
-    validators = [
-        validate_amount,
-        validate_reference,
-    ]
+
+class TransactionsGroupedbyType(serializers.Serializer):
+    """ "Serializer for transaction group by"""
+
+    total_inflow = serializers.DecimalField(max_digits=12, decimal_places=2)
+    total_outflow = serializers.DecimalField(max_digits=12, decimal_places=2)
+    user_email = serializers.EmailField(max_length=254)
