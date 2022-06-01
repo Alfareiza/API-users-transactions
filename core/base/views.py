acda0a90520777ab88468534bcb0cf4641fc5da1
Alfonso <alfareiza@gmail.com>
2022-06-01 13:39:38 -0300

    implemented endpoints for summary user_email and filter for group by type

--- core/base/views.py
+++ core/base/views.py
@@ -1,18 +1,54 @@
-from django.http import HttpResponse
+"""
+Views for the Transaction APIs
+"""
+
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from core.base.models import Transaction
-from core.base.serializers import TransactionsSerializer
+from core.base.resources import (
+    calc_summary_user,
+    group_types_by_user_email,
+)
+from core.base.serializers import (
+    TransactionsSerializer,
+    TransactionsGroupedbyType,
+)
 
 
 def home(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    """
+    Once the user try to acess to the homepage, will be redirect
+    to the API DRF Home Page.
+    """
+    return HttpResponseRedirect(reverse('base:transaction-list'))
 
 
 class TransactionsViewSet(viewsets.ModelViewSet):
-    queryset = Transaction.objects.all()
+    """View for manage transaction APIs."""
+
     serializer_class = TransactionsSerializer
+    queryset = Transaction.objects.all()
+    http_method_names = ["get", "post", "delete", "put", "head"]
+
+    def get_queryset(self):
+        if self.request.query_params.get("group_by", False) == "type":
+            queryset = Transaction.objects.raw(
+                "Select id, user_email, "
+                "SUM(CASE WHEN type == 'outflow' THEN amount ELSE 0 END) "
+                "as total_outflow,  "
+                "SUM(CASE WHEN type == 'inflow' THEN amount ELSE 0 END)"
+                "as total_inflow "
+                "from BASE_TRANSACTION group by user_email"
+            )
+            result = group_types_by_user_email(queryset)
+            self.serializer_class = TransactionsGroupedbyType
+            return result
+        else:
+            return self.queryset.order_by("created_at")
 
     def create(self, request, *args, **kwargs):
         many = isinstance(request.data, list)
@@ -20,4 +56,17 @@ class TransactionsViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
+
+
+@api_view(["GET"])
+def user_email_summary(request, user_email):
+    """Get a summary of a specific user"""
+    # http://127.0.0.1:8000/api/v1/transactions/janedoe@email.com/summary/
+    transaction = Transaction.objects.filter(user_email__iexact=user_email)
+    result = {"user_email": f"User '{user_email}' doesn't exists."}
+    if transaction:
+        result = calc_summary_user(transaction)
+    return Response(result)
