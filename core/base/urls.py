68b9336b006a12a6a3b01e8716b49ff6310ad15b
Alfonso <alfareiza@gmail.com>
2022-06-01 13:29:52 -0300

    new url for user_email summary information

--- core/base/urls.py
+++ core/base/urls.py
@@ -1,8 +1,18 @@
+"""URL mappings for the base app (transaction app)"""
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-
-from core.base.views import TransactionsViewSet
+from core.base.views import TransactionsViewSet, user_email_summary
 
 router = DefaultRouter()
-router.register(r'', TransactionsViewSet, basename='transactions')
+router.register(r"transactions", TransactionsViewSet, basename="transaction")
 
-transactions_urls = router.urls
+app_name = "base"
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path(
+        "transactions/<str:user_email>/summary/",
+        user_email_summary,
+        name="user_email_summary",
+    ),
+]
