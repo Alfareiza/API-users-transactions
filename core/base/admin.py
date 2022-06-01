332fa529241921a3c004418c0a31f056f5a22d7c
Alfonso <alfareiza@gmail.com>
2022-06-01 13:27:29 -0300

    refactoring according to pep8

--- core/base/admin.py
+++ core/base/admin.py
@@ -5,7 +5,14 @@ from .models import Transaction
 
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ("reference", "date", "amount", "type", "category", "user_email")
+    list_display = (
+        "reference",
+        "date",
+        "amount",
+        "type",
+        "category",
+        "user_email",
+    )
     list_filter = ("date", "category", "user_email")
-    search_fields = ("user_email", )
-    ordering = ("date", )
+    search_fields = ("user_email",)
+    ordering = ("date",)
--- core/base/validators.py
+++ core/base/validators.py
@@ -2,12 +2,18 @@ from rest_framework import serializers
 
 
 def validate_amount(value):
-    if value['type'].lower() == 'outflow' and value['amount'] > 0:
-        raise serializers.ValidationError({'type': 'Outflow entries can not be positive.'})
-    elif value['type'].lower() == 'inflow' and value['amount'] < 0:
-        raise serializers.ValidationError({'type': 'Inflow entries can not be negative .'})
+    if value["type"].lower() == "outflow" and value["amount"] > 0:
+        raise serializers.ValidationError(
+            {"type": "Outflow entries can not be positive."}
+        )
+    elif value["type"].lower() == "inflow" and value["amount"] < 0:
+        raise serializers.ValidationError(
+            {"type": "Inflow entries can not be negative ."}
+        )
 
 
 def validate_reference(value):
-    if not value['reference'].isnumeric():
-        raise serializers.ValidationError({'reference': 'The reference only must contain numbers.'})
+    if not value["reference"].isnumeric():
+        raise serializers.ValidationError(
+            {"reference": "The reference only must contain numbers."}
+        )
